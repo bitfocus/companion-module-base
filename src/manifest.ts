1b5714d41eb1cc6dc842b9fcd16bd8cd284fc38a
Julian Waller <git@julusian.co.uk>
2022-09-04 14:17:47 +0100

    feat: implement validation of manifest json, using the json schema

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,8 +1,22 @@
 import { ModuleManifest } from '../generated/manifest'
+import manifestSchema from '../generated/manifest-json'
+import Ajv, { ValidateFunction } from 'ajv'
 
 export { ModuleManifest }
 
+const ajv = new Ajv()
+let validator: ValidateFunction | undefined
+
 /** Validate that a manifest looks correctly populated */
 export function validateManifest(manifest: ModuleManifest): void {
-	// TODO module-lib
+	if (!validator) {
+		validator = ajv.compile(manifestSchema)
+	}
+
+	if (!validator(manifest)) {
+		const errors = validator.errors
+		if (!errors) throw new Error(`Manifest failed validation with unknown reason`)
+
+		throw new Error(`Manifest validation failed: ${JSON.stringify(errors)}`)
+	}
 }
