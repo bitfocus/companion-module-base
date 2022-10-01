afa2970ca99047c7b1648005a70c82f6d3401867
Julian Waller <git@julusian.co.uk>
2022-10-02 00:57:33 +0100

    fix: manifest validation failing once webpacked

--- scripts/wrap-json-schema.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const schema = fs.readFileSync(path.join(__dirname, '../assets/manifest.schema.json'))
-
-fs.writeFileSync(path.join(__dirname, '../generated/manifest-json.js'), `module.exports = ${schema}`)
-
-fs.writeFileSync(path.join(__dirname, '../generated/manifest-json.d.ts'), `const schema: any\nexport default schema`)
--- src/manifest.ts
+++ src/manifest.ts
@@ -1,20 +1,13 @@
 import { ModuleManifest } from '../generated/manifest'
-import manifestSchema from '../generated/manifest-json'
-import Ajv, { ValidateFunction } from 'ajv'
+// @ts-expect-error no typings
+import validateManifestSchema from '../generated/validate_manifest'
 
 export { ModuleManifest }
 
-const ajv = new Ajv()
-let validator: ValidateFunction | undefined
-
 /** Validate that a manifest looks correctly populated */
 export function validateManifest(manifest: ModuleManifest): void {
-	if (!validator) {
-		validator = ajv.compile(manifestSchema)
-	}
-
-	if (!validator(manifest)) {
-		const errors = validator.errors
+	if (!validateManifestSchema(manifest)) {
+		const errors = validateManifestSchema.errors
 		if (!errors) throw new Error(`Manifest failed validation with unknown reason`)
 
 		throw new Error(`Manifest validation failed: ${JSON.stringify(errors)}`)
