af517e453f89c9faf95707c996a181a58db97458
Julian Waller <git@julusian.co.uk>
2023-12-06 22:29:09 +0000

    fix: ensure module manifest doesn't reference template module name

--- src/manifest.ts
+++ src/manifest.ts
@@ -6,6 +6,10 @@ export { ModuleManifest, ModuleManifestMaintainer, ModuleManifestRuntime }
 
 /** Validate that a manifest looks correctly populated */
 export function validateManifest(manifest: ModuleManifest): void {
+	const manifestStr = JSON.stringify(manifest)
+	if (manifestStr.includes('companion-module-your-module-name'))
+		throw new Error(`Manifest incorrectly references template module 'companion-module-your-module-name'`)
+
 	if (!validateManifestSchema(manifest)) {
 		const errors = validateManifestSchema.errors
 		if (!errors) throw new Error(`Manifest failed validation with unknown reason`)
