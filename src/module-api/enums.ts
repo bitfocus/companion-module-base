be38363930e26b40df75b75a2720e9a8fc9f270f
Julian Waller <git@julusian.co.uk>
2024-02-17 16:27:05 +0000

    chore: update dependencies

--- src/module-api/enums.ts
+++ src/module-api/enums.ts
@@ -23,7 +23,7 @@ export namespace Regex {
 	export const BOOLEAN = '/^(true|false|0|1)$/i'
 	export const PORT =
 		'/^([1-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-4])$/'
-	export const MAC_ADDRESS = '/^(?:[a-fA-F0-9]{2}\:){5}([a-fA-F0-9]{2})$/'
+	export const MAC_ADDRESS = '/^(?:[a-fA-F0-9]{2}:){5}([a-fA-F0-9]{2})$/'
 	export const PERCENT = '/^(100|[0-9]|[0-9][0-9])$/'
 	export const FLOAT = '/^([0-9]*\\.)?[0-9]+$/'
 	export const SIGNED_FLOAT = '/^[+-]?([0-9]*\\.)?[0-9]+$/'
