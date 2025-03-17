fb546ecd96d67f4b025c96c46e086a69e74de63e
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2025-03-17 17:06:53 +1100

    export new functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './manifest.js'
 export * from './module-api/index.js'
 export * from './common/osc.js'
-export { literal, combineRgb, splitRgb, splitHsl, splitHsv, splitHex, RgbComponents, assertNever } from './util.js'
+export { literal, combineRgb, splitRgb, splitHsl, splitHsv, splitHex, RgbComponents, assertNever, parseEscapeCharacters, substituteEscapeCharacters } from './util.js'
 export * from './helpers/index.js'
 
 export { runEntrypoint } from './entrypoint.js'
