bdc96165bf7eed649220a3398469d2b1e653c3ff
Julian Waller <git@julusian.co.uk>
2025-12-18 19:53:58 +0000

    feat: rearrange into monorepo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,20 @@
 import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
 
-export default generateEslintConfig({
+const baseConfig = await generateEslintConfig({
 	enableTypescript: true,
 })
+
+const customConfig = [
+	...baseConfig,
+
+	{
+		files: ['**/__tests__/**/*', '**/__mocks__/**/*', '**/examples/**/*'],
+		rules: {
+			'n/no-extraneous-require': 'off',
+			'n/no-extraneous-import': 'off',
+			'n/no-process-exit': 'off',
+		},
+	},
+]
+
+export default customConfig
