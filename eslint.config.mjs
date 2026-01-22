bcabfbb8f773251d53eb6d6f1bcbf42ff966e2c7
Julian Waller <git@julusian.co.uk>
2026-01-22 22:23:29 +0000

    chore: fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,27 @@
 import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const baseConfig = await generateEslintConfig({
 	enableTypescript: true,
+	ignores: ['coverage/**'],
 })
 
 const customConfig = [
 	...baseConfig,
+
+	{
+		// Fixup typescript not finding the config correctly
+		files: ['**/*.ts', '**/*.cts', '**/*.mts'],
+		languageOptions: {
+			parserOptions: {
+				tsconfigRootDir: __dirname,
+				project: ['./tsconfig.json', './tsconfig.tests.json', './packages/*/tsconfig.json'],
+			},
+		},
+	},
 
 	{
 		files: ['**/__tests__/**/*', '**/__mocks__/**/*', '**/examples/**/*'],
