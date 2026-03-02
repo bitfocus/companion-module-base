3548596755bb55e67b84e74b5a7e5b97a111d7a6
Jeff Walden <jwalden@mit.edu>
2026-03-01 21:54:37 -0800

    refactor: Readd `assets/manifest.schema.json` that does nothing but reference `assets/loose-manifest.schema.json`.

--- packages/companion-module-base/tools/schema-compile.mjs
+++ packages/companion-module-base/tools/schema-compile.mjs
@@ -3,17 +3,17 @@ import { $ } from 'zx'
 import fs from 'fs'
 import Ajv2020 from 'ajv/dist/2020.js'
 import standaloneCode from 'ajv/dist/standalone/index.js'
-import manifestSchema from '../assets/loose-manifest.schema.json' with { type: 'json' }
+import manifestSchema from '../assets/manifest.schema.json' with { type: 'json' }
+import looseManifestSchema from '../assets/loose-manifest.schema.json' with { type: 'json' }
 import { fileURLToPath } from 'url'
 
 // Compile Typescript definitions from the JSON schema
-await $`json2ts --input assets/loose-manifest.schema.json --output generated/manifest.d.ts --additionalProperties=false`
+await $`json2ts --cwd assets --input assets/manifest.schema.json --output generated/manifest.d.ts --additionalProperties=false`
 
 {
 	// The generated code be in esm export format:
-	const ajv = new Ajv2020({ code: { source: true, esm: true } })
-	const validate = ajv.compile(manifestSchema)
-	const moduleCode = standaloneCode(ajv, validate)
+	const ajv = new Ajv2020({ schemas: [manifestSchema, looseManifestSchema], code: { source: true, esm: true } })
+	const moduleCode = standaloneCode(ajv, { ModuleManifest: '/assets/manifest.schema.json' })
 
 	// Now you can write the module code to file
 	const outputPath = new URL('../generated/validate_manifest.js', import.meta.url)
